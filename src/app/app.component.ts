d549d5c746aea75e689db7246f70ccd084555812
cewb <dr.cewb@gmail.com>
2022-11-06 23:54:17 +0300

    http and stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './services/http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kib-app';
+
+  constructor(private http: HttpService) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.getPhUsers().subscribe((data) => {
+      console.log(data);
+    });
+  }
+  
+
 }
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  phUsersUrl = "https://jsonplaceholder.typicode.com/users";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getPhUsers(): Observable<JSON[]>  {
+    return this.http.get<JSON[]>(this.phUsersUrl, this.httpOptions);
+  }
+}
