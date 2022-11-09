87b2b81653793f2226e58576e7000983f129c705
cewb <dr.cewb@gmail.com>
2022-11-09 15:15:18 +0300

    user list now reads from db

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -3,18 +3,18 @@ import { HttpService } from 'src/app/services/http.service';
 
 
 export interface DbUser {
-        full_name: string,
-        username: string,
-        email_address: string,
-        street: string,
-        suite: string,
-        city: string,
-        zip_code: string,
-        geo_lat: string,
-        geo_lng: string,
-        com_name: string,
-        com_catch_phrase: string,
-        com_bs: string
+  full_name: string,
+  username: string,
+  email_address: string,
+  street: string,
+  suite: string,
+  city: string,
+  zip_code: string,
+  geo_lat: string,
+  geo_lng: string,
+  com_name: string,
+  com_catch_phrase: string,
+  com_bs: string
 }
 
 @Component({
@@ -24,15 +24,25 @@ export interface DbUser {
 })
 export class UserListComponent implements OnInit {
 
-  
 
-  displayedColumns: string[] = ['name', 'username', 'email'];
+
+  displayedColumns: string[] = [
+    'full_name', 
+    'username', 
+    'email_address',
+    'street',
+    'suite',
+    'city',
+    'com_name',
+    'com_catch_phrase',
+    'com_bs'
+  ];
   dataSource = new Array<DbUser>;
 
   constructor(private http: HttpService) {
     this.http.getDbUsers().subscribe((data) => {
-      console.log(data)
-      //this.dataSource = data as unknown as Array<DbUser>;
+      //console.log(data)
+      this.dataSource = data as unknown as Array<DbUser>;
     });
   }
 
